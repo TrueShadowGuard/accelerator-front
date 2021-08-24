fbb97f465289ddd78a6a7adcec06ea74866d9545
Vadim <playvarian@gmail.com>
2021-08-24 09:02:45 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 80;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'front', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`View server has started at ${PORT}`));
