1614232a1fcc096a26a1962649d8c357aaf1c1e5
Vadim <playvarian@gmail.com>
2021-09-24 21:40:49 +0300

    add max-width to select distance slider

--- front/src/pages/Accelerator.js
+++ front/src/pages/Accelerator.js
@@ -28,60 +28,64 @@ const Accelerator = () => {
 
         <textarea rows="12" placeholder="Enter all data from the PDB file:"/>
 
-        Element to calculate
-        <div className={s.selectElementContainer}>
-          <FormControl className={s.selectedElement}>
-            <Select
-              autoWidth={true}
-              label="Element"
-              value={element || 'H'}
-              onChange={e => {
-                setElement(e.target.value)
-              }}
-            >
-              {elements.map(el => <MenuItem key={el} value={el}>{el}</MenuItem>)}
-            </Select>
-          </FormControl>
-          <button data-default={true}>
-            <img alt="Select element button"
-                 src={TableSvg}
-                 className={s.selectElementButton}
-                 width={40}
-                 height={40}
-                 onClick={() => setIsTableVisible(true)}
-            />
-          </button>
-        </div>
-
-        Max acceptable distance, Å
-        <div className={s.selectDistanceContainer}>
-          <div className={s.selectedDistance}>
-            <TextField type="number"
-                       variant="standard"
-                       value={distance || ""}
-                       onChange={e => {
-                         let number = +e.target.value;
-                         number = Math.max(minDistance, number);
-                         number = Math.min(maxDistance, number);
-                         console.log('number: ' + number);
-                         setDistance(number);
-                       }}
-            />
+        <div className={s.elementAndDistanceContainer}>
+          <div>
+            Element to calculate
+            <div className={s.selectElementContainer}>
+              <FormControl className={s.selectedElement}>
+                <Select
+                  autoWidth={true}
+                  label="Element"
+                  value={element || 'H'}
+                  onChange={e => {
+                    setElement(e.target.value)
+                  }}
+                >
+                  {elements.map(el => <MenuItem key={el} value={el}>{el}</MenuItem>)}
+                </Select>
+              </FormControl>
+              <button data-default={true}>
+                <img alt="Select element button"
+                     src={TableSvg}
+                     className={s.selectElementButton}
+                     width={40}
+                     height={40}
+                     onClick={() => setIsTableVisible(true)}
+                />
+              </button>
+            </div>
           </div>
-          <div className={s.selectDistance}>
-            <Slider min={minDistance}
-                    max={maxDistance}
-                    step={distanceStep}
-                    valueLabelDisplay="on"
-                    sx={{color: "text.primary"}}
-                    value={distance}
-                    onChange={e => {
-                      setDistance(e.target.value)
-                    }}
-            />
+          <div>
+            Max acceptable distance, Å
+            <div className={s.selectDistanceContainer}>
+              <div>
+                <TextField type="number"
+                           variant="standard"
+                           value={distance || ""}
+                           onChange={e => {
+                             let number = +e.target.value;
+                             number = Math.max(minDistance, number);
+                             number = Math.min(maxDistance, number);
+                             console.log('number: ' + number);
+                             setDistance(number);
+                           }}
+                />
+              </div>
+              <div className={s.selectDistance}>
+                <Slider min={minDistance}
+                        max={maxDistance}
+                        step={distanceStep}
+                        valueLabelDisplay="on"
+                        sx={{color: "text.primary"}}
+                        value={distance}
+                        onChange={e => {
+                          setDistance(e.target.value)
+                        }}
+                />
+              </div>
+            </div>
           </div>
         </div>
-
         <div className="buttons">
           <button type="submit">Get result</button>
           <button type="reset">Clean out</button>
